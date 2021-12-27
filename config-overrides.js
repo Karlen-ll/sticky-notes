63479eb316aca531c8c78f7f84fae571b0f8e6e0
Karlen <karlen-ll@yandex.ru>
2021-12-27 19:41:25 +0300

    Fix code style

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,16 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = function override(config, env) {
+module.exports = function override(config) {
   config.resolve.alias = {
     ...config.resolve.alias,
-    "@components": path.resolve(`./src/components`),
-    "@containers": path.resolve(`./src/containers`),
-    "@assets": path.resolve(`./src/assets`),
-    "@global": path.resolve(`./src/global`),
-    "@utils": path.resolve(`./src/utils`),
-    "@style": path.resolve(`./src/style`),
-    "@views": path.resolve(`./src/views`),
-    "@api": path.resolve(`./src/api`),
+    '@components': path.resolve(`./src/components`),
+    '@containers': path.resolve(`./src/containers`),
+    '@assets': path.resolve(`./src/assets`),
+    '@global': path.resolve(`./src/global`),
+    '@utils': path.resolve(`./src/utils`),
+    '@style': path.resolve(`./src/style`),
+    '@views': path.resolve(`./src/views`),
+    '@api': path.resolve(`./src/api`),
   };
 
   return config;
